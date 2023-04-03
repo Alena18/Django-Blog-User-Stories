18ddd7deee4ed4b6ef2f37df901025dcf704da5f
Alena18 <sun180983@gmail.com>
2023-04-03 12:03:26 +0000

    Update env.py

---
+++ env.py
@@ -0,0 +1,3 @@
+os.environ["DATABASE_URL"] = "postgres://qvcqnujp:3wguM5KLm73Xx5KGYFu-io0IJmZFe621@trumpet.db.elephantsql.com/qvcqnujp"
+os.environ["SECRET_KEY"] = "my18091983"
+os.environ["CLOUDINARY_URL"] = "cloudinary://114968985793866:odkxsr-BrXh-p_HDmPGh23p5dJU@dmie5ezad"
